86d12afd911e0765030c066aaec10121d8a20bb8
imon01 <imondar@live.com>
2018-12-23 01:41:22 -0800

    Update linkedlist.c
    Refactored and cleaned delete.

--- linkedlist.c
+++ linkedlist.c
@@ -6,36 +6,36 @@
 #include <stdbool.h>
 
 
-typedef struct node_* node;
+typedef struct node_t_* node_t;
 
-typedef struct node_ 
+typedef struct node_t_ 
 {
 	int value;
-	node next;
-}node_;
+	node_t next;
+}node_t_;
 
 
-typedef struct LinkedList_ 
+typedef struct linkedlist_t_ 
 {
-	node root;
-	node tail;
+	node_t root;
+	node_t tail;
 	int size;
-}LinkedList_;
+}linkedlist_t_;
 
 /*----------------------------------*/
 /*Local prototypes*/
 
 
 void __test_linked_list__(void);
-void __clean__(LinkedList l);
-void __test_iter_LL__(LinkedList l);
-void __test_add_LL__(LinkedList l);
-void __test_search_LL__(LinkedList l, int val);
-void __test_repop_LL__(LinkedList l, int i, int j);
-void __test_delete_LL__(LinkedList l, int i, int j);
-void __test_reverse_LL__(LinkedList l);
-void __test_merge_LL(LinkedList la, LinkedList lb);
-void __test_append_LL(LinkedList la, LinkedList lb);
+void __clean__(linkedlist_t l);
+void __test_iter_LL__(linkedlist_t l);
+void __test_add_LL__(linkedlist_t l);
+void __test_search_LL__(linkedlist_t l, int val);
+void __test_repop_LL__(linkedlist_t l, int i, int j);
+void __test_delete_LL__(linkedlist_t l, int i, int j);
+void __test_reverse_LL__(linkedlist_t l);
+void __test_merge_LL(linkedlist_t la, linkedlist_t lb);
+void __test_append_LL(linkedlist_t la, linkedlist_t lb);
 /*----------------------------------*/
 /*----------------------------------*/
 
@@ -52,22 +52,24 @@ int main(int argc, char* argv)
 /*---------Testing functions--------*/
 void __test_linked_list__(void)
 {
-	LinkedList l = malloc_ll();
-	LinkedList l2 = malloc_ll();
+	linkedlist_t l = malloc_ll();
+//	linkedlist_t l2 = malloc_ll();
 	fprintf(stderr, "malloc_ll: passed\n");
 
 	__test_add_LL__(l);
+	add_ll(l, 100);
 	__test_iter_LL__(l);
-	
+//	__test_delete_LL__(l, 0,2);
+	delete_ll(l, 0);
 //	for(int i = 0; i < 10; i++)
 //		__test_search_LL__(l, i);
-	
-	for(int i = 55; i < 62; i++)
-		add_ll(l2, i);	
-
-	add_ll(l, 100);
-	add_ll(l2, 999);
-	__test_search_LL__(l, -1);
+//	
+//	for(int i = 55; i < 62; i++)
+//		add_ll(l2, i);	
+//
+//	add_ll(l, 100);
+//	add_ll(l2, 999);
+//	__test_search_LL__(l, -1);
 //	__test_delete_LL__(l, 0,1);
 //	__test_iter_LL__(l);
 //	__test_delete_LL__(l, 2,7);
@@ -78,28 +80,28 @@ void __test_linked_list__(void)
 
 
 	__test_iter_LL__(l);
-	__test_iter_LL__(l2	);
+//	__test_iter_LL__(l2	);
 //	__test_append_LL(l, l2);	
 
-	lazymerge_ll(l, l2);
-	__test_iter_LL__(l);
-	__test_iter_LL__(l2);
-	
+//	lazymerge_ll(l, l2);
+//	__test_iter_LL__(l);
+//	__test_iter_LL__(l2);
+//	
 	printf("Final LL size: %d\n", l->size);
 	__clean__(l);
-	__clean__(l2);
+///	__clean__(l2);
 }
 
-void __test_repop_LL__(LinkedList l, int i, int j)
+void __test_repop_LL__(linkedlist_t l, int i, int j)
 {
 	while( i < j)
 		add_ll(l, i++);
 	
 }
 
-void __test_iter_LL__(LinkedList l)
+void __test_iter_LL__(linkedlist_t l)
 {
-	node curr = l->root;
+	node_t curr = l->root;
 	while( curr)
 	{
 		printf("%d ", curr->value);
@@ -109,7 +111,7 @@ void __test_iter_LL__(LinkedList l)
 	printf("\n");
 }
 
-void __test_add_LL__(LinkedList l)
+void __test_add_LL__(linkedlist_t l)
 {
 	int i  = 0;
 	for(; i < 10; i++)
@@ -118,32 +120,32 @@ void __test_add_LL__(LinkedList l)
 	}
 }	
 
-void __test_search_LL__(LinkedList l, int val)
+void __test_search_LL__(linkedlist_t l, int val)
 {
 	fprintf(stderr,"searching: %d, %s\n",val,(search_ll(l, val) == -1)?"not found":"found");
  
 }
 
-void __test_delete_LL__(LinkedList l, int i, int j)
+void __test_delete_LL__(linkedlist_t l, int i, int j)
 {
 		while( i< j)	
 			delete_ll(l, i++);
 }
 
-void __test_reverse_LL__(LinkedList l)
+void __test_reverse_LL__(linkedlist_t l)
 {
 	reverse_ll(l);
 }
 
-void __test_append_LL(LinkedList la, LinkedList lb)
+void __test_append_LL(linkedlist_t la, linkedlist_t lb)
 {
 	append_ll(la, lb);
 }
 
-void __clean__(LinkedList l)
+void __clean__(linkedlist_t l)
 {
-	node c = l->root;
-	node t = 0;
+	node_t c = l->root;
+	node_t t = 0;
 
 	while( c )
 	{
@@ -157,23 +159,23 @@ void __clean__(LinkedList l)
 /*-----------------------------*/
 /*--------LL Features----------*/
 
-LinkedList malloc_ll(void)
+linkedlist_t malloc_ll(void)
 {
-	LinkedList list = (LinkedList) malloc( sizeof(list));
+	linkedlist_t list = (linkedlist_t) malloc( sizeof(list));
 
 	if( list == 0)
 	{
-		perror("malloc: failed LinkedList creation");
+		perror("malloc: failed linkedlist_t creation");
 	}
 	return list;
 }
 
-int add_ll( LinkedList list, int value)
+int add_ll( linkedlist_t list, int value)
 {
-	node n = (node ) malloc( sizeof(n));
+	node_t n = (node_t ) malloc( sizeof(n));
 	if(n  == 0)
 	{
-		perror("malloc: failed add_ll new node");
+		perror("malloc: failed add_ll new node_t");
 	}
 
 	n->value = value;
@@ -194,15 +196,15 @@ int add_ll( LinkedList list, int value)
 	return 0;	
 }
 
-void free_ll(LinkedList list)
+void free_ll(linkedlist_t list)
 {
 	//Copy __clean__ here
 	return;
 }
 
-int search_ll( LinkedList list, int value)
+int search_ll( linkedlist_t list, int value)
 {
-	node curr = list->root;
+	node_t curr = list->root;
 	int index = 0;
 	
 	while( curr )
@@ -217,43 +219,39 @@ int search_ll( LinkedList list, int value)
 	return -1;	
 }
 
-int delete_ll( LinkedList list, int value)
+int delete_ll( linkedlist_t list, int value)
 {
-	node curr = list->root;
-	node prev = 0;
-	if( curr->value == value)
-	{
-		list-> root = list->root->next;
-		list->size--;	
-		free(curr);
-		return 0;
-	}
 
-	while( curr )
+
+	node_t indirect = list->root;
+	node_t target = 0;
+	node_t* save = &indirect;
+
+	while(indirect)
+//	while( (*indirect)->value != value)
 	{
-		if( curr->value == value)
+		target = indirect;
+		if( indirect->value == value)
 		{
-			prev->next= curr->next;
-			break;
-		}
-		prev = curr;
-		curr = curr->next;	
+			break; 
+		
+		}	
+		indirect = indirect->next;
 	}
+	list->size--;
+	indirect = indirect->next;	
+	list->root = *save;
+	free(target);	
 
-	if( curr != NULL)	
-	{
-		free(curr);	
-		list->size--;
-	}
 	return 0;	
 }
 
-void reverse_ll( LinkedList list)
+void reverse_ll( linkedlist_t list)
 {
-	node prev = 0;
-	node curr = list->root;
+	node_t prev = 0;
+	node_t curr = list->root;
 
-	node sto = 0;
+	node_t sto = 0;
 
 	while(curr)
 	{
@@ -267,7 +265,7 @@ void reverse_ll( LinkedList list)
 }
 
 
-void append_ll( LinkedList list, LinkedList list2)
+void append_ll( linkedlist_t list, linkedlist_t list2)
 {
 	if(list2 == NULL)
 		return;
@@ -277,19 +275,21 @@ void append_ll( LinkedList list, LinkedList list2)
 	list2->root = 0;
 	list->size += list2->size;
 	list2->size = 0;
+
+	free(list2);
 }
 
 
-void lazymerge_ll(LinkedList list, LinkedList list2)
+void lazymerge_ll(linkedlist_t list, linkedlist_t list2)
 {
 	if( list2 == NULL)
 		return;
 
-	node right = list2->root, prev = list->root, sto = 0;
+	node_t right = list2->root, prev = list->root, sto = 0;
 
 
 	
-	node left = list->root->next;
+	node_t left = list->root->next;
 
 	if( prev->value > right->value)
 	{
@@ -333,8 +333,20 @@ void lazymerge_ll(LinkedList list, LinkedList list2)
 	list2->root = 0;
 }
 
-void sort_ll(LinkedList l)
+void sort_ll(linkedlist_t l)
 {
-
-	node curr = list->root;
+	//TODO: implement
+//	node_t curr = l->root, max = 0;
+//
+//	int max = curr->value;
+//	curr = curr->next;
+//
+//	while(curr)	
+//	{
+//		if( curr->value > max->value)
+//		{
+//			max = curr;
+//		}
+//		curr = curr->next;
+//	}
 } 
